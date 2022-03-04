f695def8e01c128f01414d2655be5287450c7bca
sehumah <sehumah@protonmail.com>
2022-03-03 19:05:20 -0600

    get movie object out of move adapter's intent's putExtra

--- app/src/main/java/com/assignment1/flixster/MovieDetailActivity.kt
+++ app/src/main/java/com/assignment1/flixster/MovieDetailActivity.kt
@@ -2,10 +2,17 @@ package com.assignment1.flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "MovieDetailActivity"
 class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        // Get movie object out of intent's putExtra
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie  // cast as non-nullable Movie because by default it's going to be nullable movie but we know that based on how the app is designed, we should always have a valid movie extra in the intent
+        Log.i(TAG, "Movie is $movie")
+
     }
 }
